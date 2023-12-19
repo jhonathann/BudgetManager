6742e8738ef08b4e0ea8fe2c72634763024b0c65
Aleya97 <zayragomez.v@gmail.com>
2023-12-19 12:32:28 -0500

    feat(Habilidad de Agregar movimientos y leerlos en la consola. Cambios del CSS en la página NuevoMovimiento):
    Documentación clase Movimiento
    NavMenu cambio de Test a nuevoMovimiento
    Agregado NuevoMovimiento página de CSS
    
    V(0.0.2)

--- Movimiento.cs
+++ Movimiento.cs
@@ -1,10 +1,22 @@
+/// <summary>
+/// Clase movimiento encargada de definir el comportamiento de los movimientos
+/// </summary>
 public class Movimiento{
-
+        /// <summary>
+        /// Cantidad del movimiento
+        /// </summary>
+        /// <value></value>
         public int Amount {get; set;}
         
         public  string Descripcion{get; set;}
         public Categoria Tipo{get; set;}
 
+        /// <summary>
+        /// Crea un nuevo movimiento
+        /// </summary>
+        /// <param name="amount">El valor del movimiento</param>
+        /// <param name="descripcion">Descripción del movimiento</param>
+        /// <param name="tipo">Categoria del movimiento</param>
         public Movimiento(int amount, string descripcion, Categoria tipo){
             Amount = amount;
             Descripcion = descripcion;
