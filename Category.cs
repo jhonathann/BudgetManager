02c6050b7acfc2422afb7139d99924cacdf719cd
Jhonathann A. Gómez V. <jhagomezve@unal.edu.co>
2024-01-22 18:43:41 -0500

    refactor(Reorganization of files): All the classes have been moved to their own folder

--- Category.cs
+++ Category.cs
@@ -1,9 +1,10 @@
 
-    public class Category{
+public class Category
+{
     public static Dictionary<string, Category> categories = new();
-    public string Name {get; set;}
-
-    public Category(string name){
+    public string Name { get; set; }
+    public Category(string name)
+    {
         Name = name;
         categories.Add(Name, this);
     }
